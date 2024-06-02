be4ca725bc3716ce2087269f516500e25d7ffe46
BiggerH2 <161705584+BiggerH2@users.noreply.github.com>
2024-06-02 22:33:27 +0300

    updated

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,104 @@
+# lib/classes/many_to_many.py
+
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        self._title = title
+        Article.all.append(self)
+        magazine.articles.append(self)
+        author.articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Title must be a string.")
+        if not 5 <= len(value) <= 50:
+            raise ValueError("Title must be between 5 and 50 characters long.")
+        self._title = value
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
-    def magazines(self):
-        pass
-
+   
     def add_article(self, magazine, title):
-        pass
+        if magazine not in self.magazines:
+            self.magazines.append(magazine)
+        article = Article(self, magazine, title)
+        self.articles.append(article)
+        return article
+    
 
     def topic_areas(self):
-        pass
+        return list(set(article.magazine.category for article in self._articles))
 
 class Magazine:
+    """Magazine class"""
+
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not category:
+            raise ValueError("Magazine category cannot be empty.")
+        self._name = name
+        self._category = category
+        self._articles = []
 
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def category(self):
+        return self._category
+
+    @property
     def articles(self):
-        pass
+        return self._articles
 
-    def contributors(self):
-        pass
+    def add_article(self, article):
+        if article not in self._articles:
+            self._articles.append(article)
+            article.magazine = self
+
+    def remove_article(self, article):
+        if article in self._articles:
+            self._articles.remove(article)
+            article.magazine = None
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
+
+    def contributors(self):
+        return list(set(article.author for article in self._articles if article.author))
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author:
+                author_counts[author] = author_counts.get(author, 0) + 1
+        return [author for author, count in author_counts.items() if count > 2]
+
+    @classmethod
+    def top_publisher(cls):
+        if not cls._articles:
+            return None
+        return max(cls._articles, key=lambda mag: len(mag.articles))
+
+    def topic_areas(self):
+        return list(set(article.category for article in self._articles))
